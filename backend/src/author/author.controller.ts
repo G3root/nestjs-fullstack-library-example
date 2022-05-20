a74fc6e70bde9274530595ad1d4f684875a9e747
nafees nazik <nafeesnazik21@gmail.com>
2022-05-20 16:43:04 +0530

    feat: add create author controller

--- backend/src/author/author.controller.ts
+++ backend/src/author/author.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { CreateAuthorDto } from './dto/create-author.dto';
+import { Author } from './entities/author.entity';
 import { AuthorService } from './author.service';
-
 @Controller('author')
 export class AuthorController {
   constructor(private readonly authorService: AuthorService) {}
+
+  @Post()
+  create(@Body() createAuthorDto: CreateAuthorDto): Promise<Author> {
+    return this.authorService.create(createAuthorDto);
+  }
 }
