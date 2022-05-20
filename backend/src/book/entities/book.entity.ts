90c44cd13d202877f387f89f131ae1c8c6ba6e3f
nafees nazik <nafeesnazik21@gmail.com>
2022-05-20 16:12:22 +0530

    feat: add book entity

--- backend/src/book/entities/book.entity.ts
+++ backend/src/book/entities/book.entity.ts
@@ -1,1 +1,20 @@
-export class Book {}
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from 'typeorm';
+
+import { Author } from '../../author/entities/author.entity';
+@Entity()
+export class Book {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  ISBN: string;
+
+  @Column()
+  stock: number;
+}
