cd864c9d66aac5162c38433336ae7515be0bab04
nafees nazik <nafeesnazik21@gmail.com>
2022-05-20 16:55:06 +0530

    fix: add entities to typeorm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,13 +5,15 @@ import { BookModule } from './book/book.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection } from 'typeorm';
 import { AuthorModule } from './author/author.module';
-
+import { Author } from './author/entities/author.entity';
+import { Book } from './book/entities/book.entity';
 @Module({
   imports: [
     BookModule,
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'libraryDB',
+      entities: [Author, Book],
       synchronize: true,
     }),
     AuthorModule,
