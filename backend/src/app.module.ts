8d1c44471189f266bc8b26d171e468c0fbe13a59
nafees nazik <nafeesnazik21@gmail.com>
2022-05-20 15:21:03 +0530

    feat: add type orm

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [BookModule],
+  imports: [
+    BookModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'libraryDB',
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
