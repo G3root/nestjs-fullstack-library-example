3287f84dc7afd2ddb0442422d55cd8cb57b77fc4
nafees nazik <nafeesnazik21@gmail.com>
2022-05-23 09:31:41 +0530

    feat: validate book dto

--- backend/src/book/dto/create-book.dto.ts
+++ backend/src/book/dto/create-book.dto.ts
@@ -1,1 +1,22 @@
-export class CreateBookDto {}
+import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
+export class CreateBookDto {
+  @IsNotEmpty()
+  @IsString()
+  title: string;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  @IsNotEmpty()
+  @IsString()
+  ISBN: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  stock: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  author: number;
+}
