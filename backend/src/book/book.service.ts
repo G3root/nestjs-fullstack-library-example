3926325ebdbcdf109313f2ec79cc535f71bfc44c
nafees nazik <nafeesnazik21@gmail.com>
2022-05-23 09:58:59 +0530

    feat: update create book method

--- backend/src/book/book.service.ts
+++ backend/src/book/book.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { CreateBookDto } from './dto/create-book.dto';
-import { UpdateBookDto } from './dto/update-book.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Author } from 'src/author/entities/author.entity';
+import { Repository } from 'typeorm';
+import { CreateBookDto, UpdateBookDto } from './dto';
+import { Book } from './entities/book.entity';
 
 @Injectable()
 export class BookService {
+  constructor(
+    @InjectRepository(Book)
+    private readonly booksRepository: Repository<Book>,
+  ) {}
   create(createBookDto: CreateBookDto) {
-    return 'This action adds a new book';
+    const { title, description, ISBN, stock, author: authorID } = createBookDto;
+    const author = new Author();
+    author.id = authorID;
+
+    const book = new Book();
+    book.title = title;
+    book.description = description;
+    book.ISBN = ISBN;
+    book.stock = stock;
+    book.author = author;
+    return this.booksRepository.save(book);
   }
 
   findAll() {
