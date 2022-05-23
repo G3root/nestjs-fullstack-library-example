01fd619f87311567bff3cc3b5c9663a156a781e9
nafees nazik <nafeesnazik21@gmail.com>
2022-05-23 17:36:22 +0530

    feat: add table to render authors data

---
+++ frontend/src/components/authors-table.tsx
@@ -0,0 +1,86 @@
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+
+import { useHomeData } from "~/hooks";
+import { Link } from "./link";
+
+export interface AuthorsTableProps {}
+
+export function AuthorsTable(props: AuthorsTableProps) {
+  const { data, mutate } = useHomeData();
+
+  const handleDelete = async (id: number) => {
+    const req = await fetch(`http://localhost:3000/author/${id}`, {
+      method: "DELETE",
+    });
+    const res = await req.json();
+    mutate();
+  };
+  return (
+    <Box>
+      <Stack direction="row" justifyContent="space-between" px="2rem">
+        <Typography variant="h4" gutterBottom component="div">
+          Authors data
+        </Typography>
+        <Box>
+          <Button
+            variant="contained"
+            component={Link}
+            noLinkStyle
+            href="/add-authors"
+          >
+            add authors
+          </Button>
+        </Box>
+      </Stack>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>author name</TableCell>
+              <TableCell align="right">author id</TableCell>
+              <TableCell align="right">delete</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {data?.authors !== undefined
+              ? data.authors.map((author) => (
+                  <TableRow
+                    key={author.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {author.name}
+                    </TableCell>
+                    <TableCell align="right">{author.id}</TableCell>
+                    <TableCell align="right">
+                      <IconButton
+                        onClick={() => {
+                          handleDelete(author.id);
+                        }}
+                        aria-label="delete"
+                      >
+                        <DeleteIcon />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+}
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,3 +2,4 @@ export * from "./navbar";
 export * from "./books-table";
 export * from "./link";
 export * from "./layout";
+export * from "./authors-table";
