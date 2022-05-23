cffe3c2b0df06fed3657b6a18850b4f919dc0cbf
nafees nazik <nafeesnazik21@gmail.com>
2022-05-23 12:17:38 +0530

    feat: update cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://localhost:3001',
+    },
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
