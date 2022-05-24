f893a49a0a43e05036853a555cf6255f17ef2463
nafees nazik <nafeesnazik21@gmail.com>
2022-05-24 09:32:39 +0530

    feat: init user module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -8,6 +8,7 @@ import { AuthorModule } from './author/author.module';
 import { Author } from './author/entities/author.entity';
 import { Book } from './book/entities/book.entity';
 import { HomePageModule } from './home-page/home-page.module';
+import { UserModule } from './user/user.module';
 @Module({
   imports: [
     BookModule,
@@ -19,6 +20,7 @@ import { HomePageModule } from './home-page/home-page.module';
     }),
     AuthorModule,
     HomePageModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
