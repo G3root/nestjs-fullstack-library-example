06c9562b4f59026829ba75d2506cd5a5f7b2f724
nafees nazik <nafeesnazik21@gmail.com>
2022-05-24 11:11:10 +0530

    feat: add user entity in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,13 +10,14 @@ import { Book } from './book/entities/book.entity';
 import { HomePageModule } from './home-page/home-page.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { User } from './user/entities/user.entity';
 @Module({
   imports: [
     BookModule,
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'library.db',
-      entities: [Author, Book],
+      entities: [Author, Book, User],
       synchronize: true,
     }),
     AuthorModule,
