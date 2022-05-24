74a212af033993bbaf8def5339c0ccf8e6c22ac1
nafees nazik <nafeesnazik21@gmail.com>
2022-05-24 10:46:56 +0530

    feat: add sigin dto

--- backend/src/auth/dto/auth.dto.ts
+++ backend/src/auth/dto/auth.dto.ts
@@ -1,3 +1,4 @@
+import { OmitType } from '@nestjs/mapped-types';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class AuthDto {
@@ -13,3 +14,5 @@ export class AuthDto {
   @IsNotEmpty()
   password: string;
 }
+
+export class SignInDto extends OmitType(AuthDto, ['name'] as const) {}
