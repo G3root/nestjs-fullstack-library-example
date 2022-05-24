96531b388f8cf616ab2a606476d88c6a1f1dd243
nafees nazik <nafeesnazik21@gmail.com>
2022-05-24 13:05:38 +0530

    fix : update dto fields

--- backend/src/auth/dto/signin.dto.ts
+++ backend/src/auth/dto/signin.dto.ts
@@ -1,10 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SignInDto {
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-
   @IsEmail()
   @IsNotEmpty()
   email: string;
