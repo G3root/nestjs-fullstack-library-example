a16e12ba7b6767e23644eb317583e750069bf393
nafees nazik <nafeesnazik21@gmail.com>
2022-05-24 13:08:06 +0530

    feat: current user controller

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+import { GetUserDto } from './dto';
 import { UserService } from './user.service';
 
+@UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('me')
+  getUser(@GetUser() user: GetUserDto) {
+    return user;
+  }
 }
