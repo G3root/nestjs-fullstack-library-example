47646146ab67063372a85010c2cb75f8206c4b67
nafees nazik <nafeesnazik21@gmail.com>
2022-05-24 12:11:56 +0530

    fix: remove cookie parser

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,5 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,7 +8,7 @@ async function bootstrap() {
       origin: 'http://localhost:3001',
     },
   });
-  app.use(cookieParser());
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(3000);
 }
