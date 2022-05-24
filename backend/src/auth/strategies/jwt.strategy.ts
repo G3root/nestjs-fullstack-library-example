08346a250dd123a2f6f67b35f08a8633bea40736
nafees nazik <nafeesnazik21@gmail.com>
2022-05-24 10:21:53 +0530

    feat: add jwt strategy

---
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { jwtConstants } from '../constants';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private readonly userService: UserService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: jwtConstants.secret,
+    });
+  }
+
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.userService.findById(payload.sub);
+    delete user.hash;
+    return user;
+  }
+}
