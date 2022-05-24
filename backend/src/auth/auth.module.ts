40ba9a3a32b4191f24150a2a62cb7e16ef99577e
nafees nazik <nafeesnazik21@gmail.com>
2022-05-24 12:33:24 +0530

    fix: export user service

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -4,9 +4,15 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/user/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './strategies';
+import { UserModule } from 'src/user/user.module';
 @Module({
-  imports: [JwtModule.register({}), TypeOrmModule.forFeature([User])],
+  imports: [
+    JwtModule.register({}),
+    TypeOrmModule.forFeature([User]),
+    UserModule,
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
