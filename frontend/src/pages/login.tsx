1a1c161b5a7d8b71896856b9f5449e75b2225a7a
nafees nazik <nafeesnazik21@gmail.com>
2022-05-24 13:37:56 +0530

    feat: add login and sign up page

---
+++ frontend/src/pages/login.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const LoginPage: NextPage = () => {
+  return (
+    <>
+      <h1>login page</h1>
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ frontend/src/pages/signup.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const SignupPage: NextPage = () => {
+  return (
+    <>
+      <h1>signup page</h1>
+    </>
+  );
+};
+
+export default SignupPage;
